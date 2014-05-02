dc06e58795fc00e10605f58f4dccdf07028df4c1
Neil Anderson <neil.t.anderson@gmail.com>
2014-05-02 23:03:50 +0100

    provide links to nested routes from overview page

--- js/router.js
+++ js/router.js
@@ -3,18 +3,39 @@
 'use strict';
 
 TrelloApp.Router.map(function () {
-  this.resource('boards', { path: '/' });
+  this.resource('boards', { path: '/' }, function(){
+    this.route('mine');
+    this.route('other');
+    this.route('starred');
+  });
 });
 
-TrelloApp.BoardsRoute = Ember.Route.extend({
+TrelloApp.BoardsIndexRoute = Ember.Route.extend({
   model: function () {
     return this.store.find('board');
   },
   renderTemplate: function () {
     this.render();
-    this.render('boards/mine', {into: 'boards', outlet: 'myBoards'});
-    this.render('boards/other', {into: 'boards', outlet: 'other'});
-    this.render('boards/starred', {into: 'boards', outlet: 'starred'});
+    this.render('boards/mine', {into: 'boards/index', outlet: 'myBoards'});
+    this.render('boards/other', {into: 'boards/index', outlet: 'other'});
+    this.render('boards/starred', {into: 'boards/index', outlet: 'starred'});
   }
 });
 
+TrelloApp.BoardsStarredRoute = Ember.Route.extend({
+  model: function () {
+    return this.store.find('board', {isStarred : true});
+  }
+});
+
+TrelloApp.BoardsMineRoute = Ember.Route.extend({
+  model: function () {
+    return this.store.find('board', {isMine : true});
+  }
+});
+
+TrelloApp.BoardsOtherRoute = Ember.Route.extend({
+  model: function () {
+    return this.store.find('board', {isMine : false});
+  }
+});
